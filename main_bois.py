8c3b6c36e036353f89da822ed79e97e6f2b1c0e8
jlebot44 <lebot.julien@gmail.com>
2023-05-26 17:39:27 +0200

    amélioration main_bois.py

--- main_bois.py
+++ main_bois.py
@@ -3,10 +3,13 @@ import math
 import numpy as np
 
 cells_init = []
-cells = []
 
+
+#christal
 target_1 = 0
 target_2 = 0
+
+#eggs
 target_3 = 0
 
 result = ""
@@ -32,47 +35,58 @@ for i in input().split():
 
 # game loop
 while True:
+    total_ants = 0
+    cells = []
     for i in range(number_of_cells):
 
-        total_ants = 0
+        
 
         # resources: the current amount of eggs/crystals on this cell
         # my_ants: the amount of your ants on this cell
         # opp_ants: the amount of opponent ants on this cell
         resources, my_ants, opp_ants = [int(j) for j in input().split()]
 
+        cells.append([resources, my_ants, opp_ants])
+
         if ants_base == 0 and my_ants !=0:
             ants_base = i
 
-        if i == target_1 and resources == 0:
-            target_1 = 0
-        if i == target_2 and resources == 0:
-            target_2 = 0        
-        
-        if resources !=0 and target_1 == 0:
-            target_1 = i
-
-        if resources != 0 and target_1 != i and target_2 == 0:
-            target_2 = i
-
-
-
         total_ants += my_ants
 
-        cells.append([resources, my_ants, opp_ants])
+
+    for i,cell in enumerate(cells):      
 
 
-    for index,cell in enumerate(cells_init):
-        print("init_cell : ", index, cell, file=sys.stderr, flush=True)
+        if total_ants > 30:
+            if i == target_1 and cell[0] == 0 :
+                target_1 = 0
+            if i == target_2 and cell[0] == 0 :
+                target_2 = 0
+         
 
-    for index,cell in enumerate(cells):
-        print("cell : ", index, cell, file=sys.stderr, flush=True)
+            if cell[0] !=0 and target_1 == 0 and cells_init[i][0] == 2:
+                target_1 = i
 
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+            if cell[0] != 0 and target_1 != i and target_2 == 0 and cells_init[i][0] == 2:
+                target_2 = i
 
-    result = "LINE " + str(ants_base) + " " + str(target_1) + " " + str(int(my_ants/2)) + ";" +\
-             "LINE " + str(ants_base) + " " + str(target_2) + " " + str(int(my_ants/2))
+            result = "LINE " + str(ants_base) + " " + str(target_1) + " " + str(int(total_ants/2)) + ";" +\
+                    "LINE " + str(ants_base) + " " + str(target_2) + " " + str(int(total_ants/2))
+
+        else : 
+            if i == target_3 and cell[0] == 0 :
+                target_3 = 0
+
+            if cell[0] !=0 and target_1 == 0 and cells_init[i][0] == 1:
+                target_3 = i
+
+            result = "LINE " + str(ants_base) + " " + str(target_3) + " " + str(int(total_ants))  
+
+
+
+
+    # print("total_ants :", total_ants, file=sys.stderr, flush=True)
+    # print("total_ants ratio 3 :", str(int(total_ants/3)), file=sys.stderr, flush=True)
 
 
 
